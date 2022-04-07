88b3a2e11147f0e76d32d912d313a51b3cf1dc6f
Chanmoey <relijifa@gmail.com>
2022-04-07 16:00:02 +0800

    前向传播类。

---
+++ ch01/forward_net.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self):
+        self.params = []
+
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        return out

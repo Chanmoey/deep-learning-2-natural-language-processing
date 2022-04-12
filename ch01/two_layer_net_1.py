140172d02a48363f9dd17fb4268e8407640aee3a
Chanmoey <relijifa@gmail.com>
2022-04-12 21:14:26 +0800

    针对螺旋数据集的简易神经网络。

---
+++ ch01/two_layer_net_1.py
@@ -0,0 +1,45 @@
+import numpy as np
+from common.layers import Affine, Sigmoid, SoftmaxWithLoss
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O, = input_size, hidden_size, output_size
+
+        # 初始化权重和偏置
+        W1 = np.random.randn(I, H) * 0.01
+        b1 = np.random.randn(H)
+
+        W2 = np.random.randn(H, O) * 0.01
+        b2 = np.random.randn(O)
+
+        # 生成神经网络的层
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+        self.loss_layer = SoftmaxWithLoss()
+
+        # 将所有权重整理到列表中
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+
+        return x
+
+    def forward(self, x, t):
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout

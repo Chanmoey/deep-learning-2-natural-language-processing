6f9d7e2c84f9b0371e6586d41a10cecabea98f44
Chanmoey <relijifa@gmail.com>
2022-04-13 20:04:58 +0800

    预处理。

---
+++ ch02/preprocess.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+    return corpus, word_to_id, id_to_word

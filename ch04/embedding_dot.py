43c62ee86ee3d7de9d203f84bbf4fdd138eb5d0a
Chanmoey <relijifa@gmail.com>
2022-04-19 16:51:16 +0800

    EmbeddingDot层的实现。

---
+++ ch04/embedding_dot.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from ch04.embeding import Embedding
+
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+        dtarget_W = dout * h
+        dh = dout * target_W
+        return dh
